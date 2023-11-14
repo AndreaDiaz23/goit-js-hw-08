935bd1bab8cf4d59fa513398148f18a7f1a30a0c
AndreaDiaz23 <andrea.grafico23@gmail.com>
2023-11-13 22:19:37 -0500

    worktwo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
+const videoInfo = document.getElementById('vimeo-player');
+const playVimeoOk = new Vimeo(videoInfo);
+
+
+const saveTime = throttle(function () {
+    playVimeoOk.getCurrentTime().then(function (time) {
+      localStorage.setItem('videoplayer-current-time', time);
+    });
+  }, 1000);
+
+playVimeoOk.on('timeupdate', saveTime);
+
+const timeFinal = localStorage.getItem('videoplayer-current-time');
+
+if (timeFinal) {
+    playVimeoOk.setCurrentTime(parseFloat(timeFinal));
+}
